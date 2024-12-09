eb3e42a5262692b0fb52158ed3d53446cd56137b
vasanth_dev_07 <vasanthkumarj.job@gmail.com>
2024-12-09 13:03:55 +0530

    base model

--- apps/common/models/base.py
+++ apps/common/models/base.py
@@ -10,3 +10,15 @@ COMMON_BLANK_AND_NULLABLE_FEILD_CONFIG = {
     'blank':True,
     **COMMON_NULLABLE_FEILD_CONFIG
 }
+
+class BaseModel(models.Model):
+
+    # uuid for unique id field
+    uuid = models.UUIDField(default=uuid.uuid4(),editable=False)
+
+    # for time tracking
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+    is_delete = models.BooleanField(default=False)
+
